efe3ffee62c6d057d305614b2705d27e1ffd2a4e
ajorgensen89 <ajorgensen89@gmail.com>
2023-02-03 18:00:38 +0000

    Add user request lopp to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -19,16 +19,23 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10, 20, 30, 40, 50, 60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10, 20, 30, 40, 50, 60\n")
 
-    data_str = input("Enter your data here:")
-    print(f"The data provided is {data_str}")
+        data_str = input("Enter your data here:")
+        print(f"The data provided is {data_str}")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
-    print(f"one {sales_data}")
+        sales_data = data_str.split(",")
+        
+        print(f"one {sales_data}")
+
+        if validate_data(sales_data):
+            print("Date valid!")
+            break
+
+    return sales_data    
 
 
 def validate_data(values):
@@ -45,9 +52,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")  
+        return False
 
-    print(f"two {values}")    
+    return True
 
 
-get_sales_data()
-
+data = get_sales_data()
